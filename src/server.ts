27eeba9a143ff49467365012355cf028b2f7b5fc
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-17 22:52:25 +0100

    added get, post, put and delete request, working with elephant live db and postman, not checked with front end

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
-import { Client } from "pg";
+import dotenv from "dotenv"; // what is the difference between dotenv/config?
+import  pg  from "pg";//why not 'import {Client} from "pg"?
+
+import { date } from "faker"; // what is that? 
 
 
 const app = express();
 
 dotenv.config();
-const client = new Client (process.env.DATABASE_URL)
 app.use(express.json());
 app.use(cors());
 
@@ -17,13 +18,70 @@ app.use(cors());
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
 
-
 app.get("/todos", async (req, res) => {
+  // I had an error saying Client has already been connected you cannot reuse a client. St Overflow suggested to move new client to the fn
+  const client = new pg.Client (process.env.DATABASE_URL) //why pg.Client and not new Client?
   await client.connect();
   const allTodos = await client.query("SELECT * FROM todos;");
   res.status(200).json(allTodos);
-  console.log(res.status,allTodos)
-  await client.end()
+  
+  await client.end();
+});
+
+// should I add catching errors? 
+app.post("/todos", async (req,res) => {
+  const client = new pg.Client ({connectionString: process.env.DATABASE_URL}) 
+
+  await client.connect();
+  const {description, dateAdded, dueDate, status} = req.body;
+  await client.query("INSERT INTO todos (description, date_added,due_date, status) VALUES ($1, $2, $3, $4)",[description, dateAdded,dueDate,status]);
+  res.status(201).json({status:'success'});
+  await client.end();
+});
+
+app.put("/todos/:id", async (req, res) => {
+  const client = new pg.Client ({connectionString: process.env.DATABASE_URL});
+  await client.connect();
+  const id = parseInt(req.params.id);
+  console.log(id, 'id for put request')
+  const {description, dateAdded, dueDate, status} = req.body;
+  console.log({description, dateAdded, dueDate, status}, 'logging form put request')
+  const result  = await client.query ("UPDATE todos SET description = $2, date_added = $3, due_date = $4, status = $5 WHERE id = $1;",[id, description, dateAdded, dueDate, status]);
+  if(result.rowCount === 1) {
+    res.status(200).json({
+      status: "success"
+    })
+  }else {
+    res.status(404).json({
+      status: "fail"
+    })
+  }
+  await client.end();
+})
+
+app.delete("/todos/:id", async (req, res) => {
+  const client = new pg.Client ({connectionString: process.env.DATABASE_URL});
+  await client.connect();
+  const id = parseInt(req.params.id);
+  console.log(id)
+  const queryResult = await client.query ("DELETE FROM todos WHERE id=$1;",[id])
+  console.log(queryResult)
+  const didRemove = queryResult.rowCount === 1;
+
+  if (didRemove) {
+    
+    res.status(200).json({
+      status: "success",
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      data: {
+        id: "Could not find a signature with that id identifier",
+      },
+    });
+  }
+  await client.end();
 });
 
 // POST /items
