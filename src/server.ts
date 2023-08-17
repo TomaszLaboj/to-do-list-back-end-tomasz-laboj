d2a7ce184deef7b9d77f826c29ea0f4d7da7a1e1
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-17 22:55:37 +0100

    formatted and linted

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"; // what is the difference between dotenv/config?
-import  pg  from "pg";//why not 'import {Client} from "pg"?
+import pg from "pg"; //why not 'import {Client} from "pg"?
 
-import { date } from "faker"; // what is that? 
-
+// import { date } from "faker"; // what is that?
 
 const app = express();
 
@@ -12,64 +11,70 @@ dotenv.config();
 app.use(express.json());
 app.use(cors());
 
-
-
-
 const PORT_NUMBER = process.env.PORT ?? 4000;
-
 
 app.get("/todos", async (req, res) => {
   // I had an error saying Client has already been connected you cannot reuse a client. St Overflow suggested to move new client to the fn
-  const client = new pg.Client (process.env.DATABASE_URL) //why pg.Client and not new Client?
+  const client = new pg.Client(process.env.DATABASE_URL); //why pg.Client and not new Client?
   await client.connect();
   const allTodos = await client.query("SELECT * FROM todos;");
   res.status(200).json(allTodos);
-  
+
   await client.end();
 });
 
-// should I add catching errors? 
-app.post("/todos", async (req,res) => {
-  const client = new pg.Client ({connectionString: process.env.DATABASE_URL}) 
+// should I add catching errors?
+app.post("/todos", async (req, res) => {
+  const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
 
   await client.connect();
-  const {description, dateAdded, dueDate, status} = req.body;
-  await client.query("INSERT INTO todos (description, date_added,due_date, status) VALUES ($1, $2, $3, $4)",[description, dateAdded,dueDate,status]);
-  res.status(201).json({status:'success'});
+  const { description, dateAdded, dueDate, status } = req.body;
+  await client.query(
+    "INSERT INTO todos (description, date_added,due_date, status) VALUES ($1, $2, $3, $4)",
+    [description, dateAdded, dueDate, status]
+  );
+  res.status(201).json({ status: "success" });
   await client.end();
 });
 
 app.put("/todos/:id", async (req, res) => {
-  const client = new pg.Client ({connectionString: process.env.DATABASE_URL});
+  const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
   await client.connect();
   const id = parseInt(req.params.id);
-  console.log(id, 'id for put request')
-  const {description, dateAdded, dueDate, status} = req.body;
-  console.log({description, dateAdded, dueDate, status}, 'logging form put request')
-  const result  = await client.query ("UPDATE todos SET description = $2, date_added = $3, due_date = $4, status = $5 WHERE id = $1;",[id, description, dateAdded, dueDate, status]);
-  if(result.rowCount === 1) {
+  console.log(id, "id for put request");
+  const { description, dateAdded, dueDate, status } = req.body;
+  console.log(
+    { description, dateAdded, dueDate, status },
+    "logging form put request"
+  );
+  const result = await client.query(
+    "UPDATE todos SET description = $2, date_added = $3, due_date = $4, status = $5 WHERE id = $1;",
+    [id, description, dateAdded, dueDate, status]
+  );
+  if (result.rowCount === 1) {
     res.status(200).json({
-      status: "success"
-    })
-  }else {
+      status: "success",
+    });
+  } else {
     res.status(404).json({
-      status: "fail"
-    })
+      status: "fail",
+    });
   }
   await client.end();
-})
+});
 
 app.delete("/todos/:id", async (req, res) => {
-  const client = new pg.Client ({connectionString: process.env.DATABASE_URL});
+  const client = new pg.Client({ connectionString: process.env.DATABASE_URL });
   await client.connect();
   const id = parseInt(req.params.id);
-  console.log(id)
-  const queryResult = await client.query ("DELETE FROM todos WHERE id=$1;",[id])
-  console.log(queryResult)
+  console.log(id);
+  const queryResult = await client.query("DELETE FROM todos WHERE id=$1;", [
+    id,
+  ]);
+  console.log(queryResult);
   const didRemove = queryResult.rowCount === 1;
 
   if (didRemove) {
-    
     res.status(200).json({
       status: "success",
     });
