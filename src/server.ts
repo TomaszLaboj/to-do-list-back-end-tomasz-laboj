23a1adb745ddb0d182bb1eaafcfd3fa1b6f37dca
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-17 15:03:16 +0100

    connected to elephant db, only 1 get request

--- src/server.ts
+++ src/server.ts
@@ -1,79 +1,69 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {
-  addDbItem,
-  deleteDbItemById,
-  getAllDbItems,
-  getDbItemById,
-  DbItem,
-  updateDbItemById,
-} from "./db";
-import filePath from "./filePath";
+import { Client } from "pg";
+
 
 const app = express();
 
-/** Parses JSON data in a request automatically */
+dotenv.config();
+const client = new Client (process.env.DATABASE_URL)
 app.use(express.json());
-/** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
 app.use(cors());
 
-// read in contents of any environment variables in the .env file
-dotenv.config();
 
-// use the environment variable PORT, or 4000 as a fallback
+
+
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
-// API info page
-app.get("/", (req, res) => {
-  const pathToFile = filePath("../public/index.html");
-  res.sendFile(pathToFile);
-});
 
-// GET /items
-app.get("/items", (req, res) => {
-  const allSignatures = getAllDbItems();
-  res.status(200).json(allSignatures);
+
+app.get("/todos", async (req, res) => {
+  await client.connect();
+  const allTodos = await client.query("SELECT * FROM todos;");
+  res.status(200).json(allTodos);
+  console.log(res.status,allTodos)
+  await client.end()
 });
 
 // POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdSignature = addDbItem(postData);
-  res.status(201).json(createdSignature);
-});
+// app.post<{}, {}, DbItem>("/items", (req, res) => {
+//   // to be rigorous, ought to handle non-conforming request bodies
+//   // ... but omitting this as a simplification
+//   const postData = req.body;
+//   const createdSignature = addDbItem(postData);
+//   res.status(201).json(createdSignature);
+// });
 
-// GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // GET /items/:id
+// app.get<{ id: string }>("/items/:id", (req, res) => {
+//   const matchingSignature = getDbItemById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
-// DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = deleteDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // DELETE /items/:id
+// app.delete<{ id: string }>("/items/:id", (req, res) => {
+//   const matchingSignature = deleteDbItemById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-  const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // PATCH /items/:id
+// app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
+//   const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
